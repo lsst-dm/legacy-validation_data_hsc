545c4fb56cd1a4e992c454d056af04d8a23f40a9
Michael Wood-Vasey <wmwv@pitt.edu>
2018-06-19 15:17:33 -0500

    Add config to not apply ubercal.
    Because we don't calculate meas_mosaic here.

---
+++ config/hscCoadd.py
@@ -0,0 +1,6 @@
+# Apply meas_mosaic ubercal results to input calexps?
+config.makeCoaddTempExp.doApplyUberCal=False
+config.makeCoaddTempExp.doApplySkyCorr=False
+
+config.assembleCoadd.doApplyUberCal=False 
+config.assembleCoadd.assembleStaticSkyModel.doApplyUberCal=False
