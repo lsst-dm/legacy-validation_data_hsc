99fd14af19b8c53c28fbe7631a355caca70d703f
Michael Wood-Vasey <wmwv@pitt.edu>
2018-09-07 15:38:42 -0500

    Remove attachTransmission=True from config as is obs_subaru default.

--- config/singleFrameDriverConfig.py
+++ config/singleFrameDriverConfig.py
@@ -1,4 +1,2 @@
-# We do not have transmission curves attached to our validation repos yet
-config.processCcd.isr.doAttachTransmissionCurve = True
 # these commissioning data do not have the correct header info to apply the stray light correction
 config.processCcd.isr.doStrayLight = False
