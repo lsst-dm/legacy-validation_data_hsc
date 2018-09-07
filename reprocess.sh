70a29ee269c71f1e0134c92555393bcf4c1b7a13
Michael Wood-Vasey <wmwv@pitt.edu>
2018-09-07 15:37:37 -0500

    Update examples and config to use HTM PS1 catalogs.

--- reprocess.sh
+++ reprocess.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 source /software/lsstsw/stack/loadLSST.bash
-# Pick up pipe_drivers in lsst_distrib
-setup lsst_distrib -t w_2018_22  # which was v16.0.rc1
 # Set up our specific camera
-setup obs_subaru -t w_2018_22  # which was v16.0.rc1
-
+# w_2018_35 was the weekly before the new HTM-based PS1 catalogs were merged
+setup obs_subaru -t w_2018_35
+# Add meas_extensions_photometryKron module to do Kron photometry.
+setup meas_extensions_photometryKron -t w_2018_35
+# Set up this specific repo just so we can generically refer to VALIDATION_DATA_HSC_DIR below.
+setup -k -r validation_data_hsc
+#
 # Setup
 export OMP_NUM_THREADS=1
 
-# Fake out the pipeline about the origin of the reference catalog
-export SETUP_ASTROMETRY_NET_DATA="astrometry_net_data sdss-dr9-fink-v5b"
-export ASTROMETRY_NET_DATA_DIR=sdss-dr9-fink-v5b
-
 # Ingest raw data into repo
-DATA="DATA"
+DATA="data"
 mkdir "${DATA}"
 echo lsst.obs.hsc.HscMapper > "${DATA}"/_mapper
-ingestImages.py "${DATA}" --mode=link 'raw/*.fits'
+ingestImages.py "${DATA}" --mode=link "${VALIDATION_DATA_HSC_DIR}"/raw/*.fits
 
 # Copy in the HSC transmission files (atmosphere+optics+sensors).
 cp -pr transmission "${DATA}"
+
+# Link in the reference catalogs
+ln -s "${VALIDATION_DATA_HSC_DIR}"/ref_cats "${DATA}"/ref_cats
 
 # Heavy lifting
 singleFrameDriver.py "${DATA}" --calib CALIB --output "${DATA}" -C config/singleFrameDriverConfig.py --job singleFrame --cores 16 --id ccd=0..103 visit=903982^904006^904828^904846^903332^903340^904350^904378
